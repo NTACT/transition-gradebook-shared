32bd0be285dcf63f9a8e93117d7fb7f43225f0b9
James Meyers <james.meyers@emberex.com>
2018-10-15 13:32:24 -0700

    Add files.

---
+++ index.js
@@ -0,0 +1,8 @@
+
+module.exports = {
+  convertGradeToGpa: require('./lib/convertGradeToGpa'),
+  enums: require('./lib/enums'),
+  getGpaRange: require('./lib/getGpaRange'),
+  normalizeGpa: require('./lib/normalizeGpa'),
+  riskUtils: require('./lib/riskUtils'),
+};
